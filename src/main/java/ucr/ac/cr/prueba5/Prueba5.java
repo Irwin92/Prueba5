cf8a2721be6d336bba565f09cb2666c7a6daaef6
USUARIO <USUARIO@DESKTOP-TCNQ62D>
2024-03-13 11:28:04 -0600

    primera prueba

---
+++ src/main/java/ucr/ac/cr/prueba5/Prueba5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.prueba5;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Prueba5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
